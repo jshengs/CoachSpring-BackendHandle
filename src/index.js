96a936ef056c9adb52928baa9baa8eafd7a2426c
Jun <156655601+jshengs@users.noreply.github.com>
2024-03-11 20:24:46 +0800

    push tag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { createCanvas, loadImage } = require("canvas");
-const canvas = createCanvas(644, 920);
+const canvas = createCanvas(710, 1064);
 const ctx = canvas.getContext("2d");
 const fs = require("fs");
 let cloudImg, logoImg, tagImg;
@@ -10,26 +10,26 @@ async function loadAssets(color) {
 
   switch (color) {
     case "purpleVideo":
-      tagImg = await loadImage("uploads/01_visionary.png");
+      tagImg = await loadImage("uploads/T_visionary.png");
       break;
 
     case "denimVideo":
-      tagImg = await loadImage("uploads/02_explorer.png");
+      tagImg = await loadImage("uploads/T_explorer.png");
       break;
 
     case "blackVideo":
-      tagImg = await loadImage("uploads/03_activist.png");
+      tagImg = await loadImage("uploads/T_activist.png");
       break;
 
     case "yellowVideo":
-      tagImg = await loadImage("uploads/04_creative.png");
+      tagImg = await loadImage("uploads/T_creative.png");
       break;
 
     case "tyeDyeVideo":
-      tagImg = await loadImage("uploads/05_lover.png");
+      tagImg = await loadImage("uploads/T_lover.png");
       break;
     default:
-      tagImg = await loadImage("uploads/04_creative.png");
+      tagImg = await loadImage("uploads/T_creative.png");
   }
 }
 
@@ -40,17 +40,17 @@ async function Main(name, color) {
     throw new Error("Failed to load images.");
   }
 
-  ctx.drawImage(cloudImg, 0, 0, 644, 920);
+  ctx.drawImage(cloudImg, 0, 0, 710, 1064);
 
   const logoX = (canvas.width - logoImg.width) / 2;
   ctx.drawImage(logoImg, logoX, 45);
 
   ctx.drawImage(
     tagImg,
-    canvas.width * 0.5 - 710 * 0.8 * 0.5,
-    canvas.height * 0.7 - 523 * 0.8 * 0.5,
-    710 * 0.8,
-    523 * 0.8
+    canvas.width * 0.5 - 675 * 0.8 * 0.5,
+    canvas.height * 0.62 - 503 * 0.8 * 0.5,
+    675 * 0.8,
+    503 * 0.8
   );
 
   ctx.font = "bold 100px 'Helvetica LT Pro Bold'";
